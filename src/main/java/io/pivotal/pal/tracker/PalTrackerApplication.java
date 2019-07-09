7974ff80aa3da04d51933f7d7e96ca23912bc38f
Irina <iullric1@ford.com>
2019-07-09 17:10:35 +0100

    Used @repository annotation instead of @bean annotation

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -2,7 +2,6 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -10,11 +9,6 @@ public class PalTrackerApplication {
 
     public static void main(String[] args){
         SpringApplication.run(PalTrackerApplication.class, args);
-    }
-
-    @Bean
-    public TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
     }
 
 }
